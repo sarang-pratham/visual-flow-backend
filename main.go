8deb2644525b17ad47681388df85d7119bc692d3
Sarang Pratham <68158794+sarang-pratham@users.noreply.github.com>
2024-09-22 17:57:46 +0530

    Swagger integration and logging middleware (#1)
    Co-authored-by: sarang-pratham<sarangpratham720@gmail.com>

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"github.com/sarang-pratham/visual-flow-backend/internal/handlers"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+// @title Visual Flow APIs
+// @version 1.0
+// @description APIs for all the calculations/features.
+// @termsOfService http://swagger.io/terms/
+// @BasePath /api/v1
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	api := e.Group("visual-flow-service/api/v1")
 	cfg := config.LoadConfig()
-	handlers.RegisterRoutes(e)
+
+	handlers.RegisterRoutes(api)
 	e.Logger.Fatal(e.Start(cfg.ServerPort))
 }
